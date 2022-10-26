13eac8f4173a1c8403b97de1050746778a741b0e
Jeongah <ji9390852@gmail.com>
2022-10-26 10:20:52 +0900

    [생성] RefTest.cpp

---
+++ Homework/RefTest/RefTest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 20;
+    int& aRef = a;
+    cout<<"a의 값 : "<<a<<endl;
+    cout<<"aRef가 참조하는 값 : "<<aRef<<endl<<endl;
+    aRef = 100;
+    cout<<"a의 값 : "<<a<<endl;
+    aRef = b;
+    cout<<"a의 값 : "<<a<<endl;
+    return 0;
+}
